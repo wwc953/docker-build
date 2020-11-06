e6dca3e3710e9795231baad9de1f1c1a6020807f
wangwc <wwc953@163.com>
2020-11-06 10:00:28 +0800

    修改lua

--- src/main/resources/lua/rpopList.lua
+++ src/main/resources/lua/rpopList.lua
@@ -1,10 +1,8 @@
 --返回List
 local array = {}
-local arrayLength = 1
-if (redis.call("llen", KEYS[1]) > tonumber(ARGV[1])) then
+local arrayLength = redis.call("llen", KEYS[1])
+if (arrayLength > tonumber(ARGV[1])) then
     arrayLength = tonumber(ARGV[1])
-else
-    arrayLength = redis.call("llen", KEYS[1])
 end
 for i = 1, arrayLength do
     array[i] = redis.call("rpop", KEYS[1])
